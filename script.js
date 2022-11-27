f69651cf27ab326783acd4882d355c91baaa1249
Sara Szwejkowska <saraszw@op.pl>
2022-11-27 15:06:49 +0100

    new js

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ firstButton.addEventListener('click', () => {
 	mothDay.style.display = 'none';
 	weekDay.style.display = 'none';
 });
+
+secondButton.addEventListener = document.querySelector('click', () => {
+	weekDay.classList.remove('hidden');
+	mothDay.style.display = 'none';
+	days.style.display = 'none'
+});
+thirdButton.addEventListener = document.querySelector('click', () => {
+	mothDay.classList.remove('hidden')
+	days.style.display = 'none';
+	weekDay.style.display = 'none'
+})
